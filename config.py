de6b039b1a84f39363a806fd4f4ada03e5d37b45
aarav669281 <aaravmishra6691@gmail.com>
2024-11-05 17:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,6 @@ API_HASH = int(os.environ.get("API_HASH", "f9064b91dc1331fe9cd614a34eb37de0")
 
 #Database 
 DB_URI = int(os.environ.get("DB_URI", "mongodb+srv://Ankit:<db_password>@cluster0.aigsy.mongodb.net/")
+
+#stringsession
+String = int(os.environ.get("BAGG_5kAZan_K8yrVJG6Msq8r4fZOcg4y0RpMwDJrrRicXTNp4YvPf0ibO0fR2sTzy9viS3S067Z0Dm85W01iKlvFxKPsX2P7AHc6uXVijTDQcb9NptGxxTqI-cuatrvqbkci8an-axmhshtuClLH5xZj9Z255jZnh3k3psXAaDAQe6pNPHd_WomX9E5CgDK4Djqmws6V8X1GVv5iuuPK4Rrtg0FnbXJ8CWaGuC5rJQGuvpiH-hoYKrcPmwMdWkCcXlUxFWRlJimJhJp-K9zU7wvrs8g-YppJocd7eXPzudvY9fmF1AlrEMkNs6MtkK3DXrk_RSHl_-gJ4tJHlpNYruzFnjXeAAAAAGqEATGAQ")            
